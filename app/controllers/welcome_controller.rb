ec6d3d2dc3784ceb22ada8992e41242675798813
gullpaperdelivery <brendanmclaughlin20@gmail.com>
2017-09-05 14:31:16 -0400

    contact and about pages and links to demos

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,18 @@
 class WelcomeController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :terms]
+  skip_before_action :authenticate_user!, only: [:index, :terms, :about, :contact]
 
   def index
   end
 
   def terms
+    #code
+  end
+
+  def about
+    #code
+  end
+
+  def contact
     #code
   end
 
